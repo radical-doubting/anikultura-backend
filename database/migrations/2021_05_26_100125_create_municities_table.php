8d9b0fbdec8043754a45e469cce62ac65e25e6e7
jg-rivera <rivera.jga10@gmail.com>
2021-05-26 18:04:48 +0800

    Added migrations for regions, provinces, municities

---
+++ database/migrations/2021_05_26_100125_create_municities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMunicitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('municities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('region_id');
+            $table->foreign('region_id')
+                ->references('id')
+                ->on('regions');
+
+            $table->unsignedBigInteger('province_id');
+            $table->foreign('province_id')
+                ->references('id')
+                ->on('provinces');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('municities');
+    }
+}
