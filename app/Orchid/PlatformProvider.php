6140682f86c001c3842ea835459de0cbf3c9a6de
jg-rivera <rivera.jga10@gmail.com>
2021-06-01 23:20:01 +0800

    Removed unnecessary menu items

--- app/Orchid/PlatformProvider.php
+++ app/Orchid/PlatformProvider.php
@@ -26,61 +26,17 @@ class PlatformProvider extends OrchidServiceProvider
     public function registerMainMenu(): array
     {
         return [
-            Menu::make('Example screen')
-                ->icon('monitor')
-                ->route('platform.example')
-                ->title('Navigation')
-                ->badge(function () {
-                    return 6;
-                }),
-
-            Menu::make('Dropdown menu')
-                ->icon('code')
+            Menu::make('Site Management')
+                ->icon('location-pin')
                 ->list([
-                    Menu::make('Sub element item 1')->icon('bag'),
-                    Menu::make('Sub element item 2')->icon('heart'),
+                    Menu::make('Regions')
+                        ->icon('location-pin')
+                        ->route('platform.sites.regions'),
+                    Menu::make('Provinces')
+                        ->icon('location-pin'),
+                    Menu::make('Municities')
+                        ->icon('location-pin'),
                 ]),
-
-            Menu::make('Basic Elements')
-                ->title('Form controls')
-                ->icon('note')
-                ->route('platform.example.fields'),
-
-            Menu::make('Advanced Elements')
-                ->icon('briefcase')
-                ->route('platform.example.advanced'),
-
-            Menu::make('Text Editors')
-                ->icon('list')
-                ->route('platform.example.editors'),
-
-            Menu::make('Overview layouts')
-                ->title('Layouts')
-                ->icon('layers')
-                ->route('platform.example.layouts'),
-
-            Menu::make('Chart tools')
-                ->icon('bar-chart')
-                ->route('platform.example.charts'),
-
-            Menu::make('Cards')
-                ->icon('grid')
-                ->route('platform.example.cards')
-                ->divider(),
-
-            Menu::make('Documentation')
-                ->title('Docs')
-                ->icon('docs')
-                ->url('https://orchid.software/en/docs'),
-
-            Menu::make('Changelog')
-                ->icon('shuffle')
-                ->url('https://github.com/orchidsoftware/platform/blob/master/CHANGELOG.md')
-                ->target('_blank')
-                ->badge(function () {
-                    return Dashboard::version();
-                }, Color::DARK()),
-
             Menu::make(__('Users'))
                 ->icon('user')
                 ->route('platform.systems.users')
