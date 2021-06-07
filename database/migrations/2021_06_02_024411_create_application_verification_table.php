8ac1d89989692da192bb6eaa6b3d87f08b7fed10
jg-rivera <rivera.jga10@gmail.com>
2021-06-07 15:24:38 +0800

    Pluralization of class names in migrations fix

--- database/migrations/2021_06_02_024411_create_application_verification_table.php
+++ database/migrations/2021_06_02_024411_create_application_verification_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateApplicationVerificationTable extends Migration
+class CreateApplicationVerificationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateApplicationVerificationTable extends Migration
      */
     public function up()
     {
-        Schema::create('application_verification', function (Blueprint $table) {
+        Schema::create('application_verifications', function (Blueprint $table) {
             $table->id();
             $table->string('verified_by');
             $table->string('position');
@@ -32,6 +32,6 @@ class CreateApplicationVerificationTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('application_verification');
+        Schema::dropIfExists('application_verifications');
     }
 }
--- database/migrations/2021_06_06_043245_create_farmland_types_table.php
+++ database/migrations/2021_06_06_043245_create_farmland_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFarmlandTypeTable extends Migration
+class CreateFarmlandTypesTable extends Migration
 {
     /**
      * Run the migrations.
