2927e21a577333a886cb5796738c85221f8420bd
jay1-53 <sadaanton@student.apc.edu.ph>
2021-06-10 17:35:57 +0800

    Updated farmer reports migration

---
+++ database/migrations/2021_06_06_155928_create_farmer_report.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFarmerReport extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+public function up()
+    {
+        Schema::create('farmer_reports', function (Blueprint $table) {
+            $table->id();
+            $table->integer('farmer_profiles_id')
+                ->references('id')
+                ->on('farmer_profiles');
+            $table->integer('seed_stages_id')
+                ->references('id')
+                ->on('seed_stages');
+            $table->integer('farmland_id')
+                ->references('id')
+                ->on('farmlands');
+            $table->integer('crop_id');
+            $table->double('volume');
+            $table->rememberToken();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('farmer_reports');
+    }
+}
