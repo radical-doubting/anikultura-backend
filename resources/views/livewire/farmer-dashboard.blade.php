59c4ac82178840ec994e24af422f3993815ef8a8
jay1-53 <sadaanton@student.apc.edu.ph>
2021-06-11 12:37:53 +0800

    Livewire View Dashboard

---
+++ resources/views/livewire/farmer-dashboard.blade.php
@@ -0,0 +1,15 @@
+<div class="text-center mb-8">
+    <img src="https://cdn.discordapp.com/attachments/719478925700497842/850287856294297610/farr.png" style="height: 250px; margin: auto auto" />
+    @foreach ($reports as $report)
+        crop id: {{ $report->crop_id }}
+        seed stages: {{ $report->seed_stages_id }}
+    <button class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-8 md:my-8" 
+        wire:click="advance_stage({{ $report->farmer_profiles_id }}, {{ $report->seed_stages_id }})">
+        text here
+    </button>
+    <button class="bg-red-500 hover:bg-blue-700 text-white font-bold rounded-full py-2 self-center px-4 items-center">
+        icon here
+    </button>
+    @endforeach
+
+</div>
