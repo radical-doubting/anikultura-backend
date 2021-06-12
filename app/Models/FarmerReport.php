97a28728a98ecf2ecb8b4f8aa71a6ec41959e5a7
jay1-53 <sadaanton@student.apc.edu.ph>
2021-06-12 22:09:09 +0800

    Aligned FarmerReport Model naming convention

---
+++ app/Models/FarmerReport.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FarmerReport extends Model
+{
+    use Filterable, HasFactory;
+
+    protected $fillable = [
+        'farmer_profile_id',
+        'seed_stage_id',
+        'farmland_id',
+        'crop_id',
+        'volume',
+    ];
+
+    protected $allowedFilters = [
+        'id',
+        'farmer_profile_id',
+    ];
+
+    protected $allowedSorts = [
+        'id',
+        'updated_at',
+        'created_at',
+    ];
+
+    public function farmer_profile() {
+        return $this->belongsTo(FarmerProfile::class);
+    }
+
+    public function seed_stage() {
+        return $this->belongsTo(SeedStage::class);
+    }
+}
