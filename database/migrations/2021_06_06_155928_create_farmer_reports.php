43676083824048a66563c16ba1b81937dcbc7c53
jay1-53 <sadaanton@student.apc.edu.ph>
2021-06-12 22:10:26 +0800

    Fixed farmer reports migration column names

--- database/migrations/2021_06_06_155928_create_farmer_reports.php
+++ database/migrations/2021_06_06_155928_create_farmer_reports.php
@@ -11,30 +11,31 @@ class CreateFarmerReports extends Migration
      *
      * @return void
      */
-public function up()
+    public function up()
     {
         Schema::create('farmer_reports', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('farmer_profiles_id')
-            ->nullable();
-            $table->foreign('farmer_profiles_id')
+            
+            $table->unsignedBigInteger('farmer_profile_id')
+                ->nullable();
+            $table->foreign('farmer_profile_id')
                 ->references('id')
                 ->on('farmer_profiles');
 
-            $table->unsignedBigInteger('seed_stages_id')
-            ->nullable();
-            $table->foreign('seed_stages_id')
+            $table->unsignedBigInteger('seed_stage_id')
+                ->nullable();
+            $table->foreign('seed_stage_id')
                 ->references('id')
                 ->on('seed_stages');
 
             $table->unsignedBigInteger('farmland_id')
-            ->nullable();
+                ->nullable();   
             $table->foreign('farmland_id')
                 ->references('id')
                 ->on('farmlands');
 
             $table->unsignedBigInteger('crop_id')
-            ->nullable();
+                ->nullable();
             $table->foreign('crop_id')
                 ->references('id')
                 ->on('crops');
@@ -42,7 +43,6 @@ public function up()
             $table->double('volume');
             $table->timestamps();
         });
-
     }
 
     /**
