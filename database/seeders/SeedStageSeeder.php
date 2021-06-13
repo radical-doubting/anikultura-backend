76174398968f21379bc769554d21f1a61a4cd52e
jay1-53 <sadaanton@student.apc.edu.ph>
2021-06-14 02:41:09 +0800

    Added final stage

--- database/seeders/SeedStageSeeder.php
+++ database/seeders/SeedStageSeeder.php
@@ -19,10 +19,11 @@ class SeedStageSeeder extends Seeder
         $seedstages = [
             ['id' => 1, 'name' => 'Nakuha ko na ang Starter Kit', 'image' => 'stage1.png'],
             ['id' => 2, 'name' => 'Natanim ko na ang mga buto', 'image' => 'stage2.png'],
-            ['id' => 3, 'name' => 'Seeds Established', 'image' => 'stage3.png'],
-            ['id' => 4, 'name' => 'Vegetative Seeds', 'image' => 'stage4.png'],
-            ['id' => 5, 'name' => 'Yield Formation Stage', 'image' => 'stage5.png'],
-            ['id' => 6, 'name' => 'Ripening Stage', 'image' => 'stage6.png'],
+            ['id' => 3, 'name' => 'Nailipat na ang buto sa lupang tataniman', 'image' => 'stage3.png'],
+            ['id' => 4, 'name' => 'Malalago at malapit na magbunga ang tanim', 'image' => 'stage4.png'],
+            ['id' => 5, 'name' => 'Namumulaklak at unti-unting nagbubunga ang tanim', 'image' => 'stage5.png'],
+            ['id' => 6, 'name' => 'Nagbunga na ang mga tanim at malapit na anihin', 'image' => 'stage6.png'],
+            ['id' => 7, 'name' => 'Naani ko na aking mga tanim', 'image' => 'stage7.png'],
         ];
 
         SeedStage::insert($seedstages);
