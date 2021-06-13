393b83b7c6ac3b1ee249f05976ed6e6754e14594
jay1-53 <sadaanton@student.apc.edu.ph>
2021-06-14 02:14:23 +0800

    Created batch seeder

---
+++ database/seeders/BatchSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Batch\Batch;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class BatchSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $date_now = Carbon::now();
+        $batches = [
+            [
+                'id' => 1,
+                'assigned_farmschool_name' => 'Mabuhay High School',
+                'region_id' => 5,
+                'province_id' => 1,
+                'municity_id' => 1,
+                'barangay' => 'Nagbalon',
+                'number_seeds_distributed' => 200,
+                'created_at' => $date_now,
+                'updated_at' => $date_now
+            ],
+            [
+                'id' => 2,
+                'assigned_farmschool_name' => 'Masagana Community School',
+                'region_id' => 5,
+                'province_id' => 1,
+                'municity_id' => 1,
+                'barangay' => 'Liputan',
+                'number_seeds_distributed' => 154,
+                'created_at' => $date_now,
+                'updated_at' => $date_now
+            ]
+        ];
+
+        Batch::insert($batches);
+    }
+}
