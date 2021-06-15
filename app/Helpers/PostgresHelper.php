986664fa2de7b701a02d627e98fac66230b1c5a7
jg-rivera <rivera.jga10@gmail.com>
2021-06-15 12:38:06 +0800

    Postgres helper tweak

--- app/Helpers/PostgresHelper.php
+++ app/Helpers/PostgresHelper.php
@@ -10,7 +10,7 @@ class PostgresHelper
     {
         if (env('DB_CONNECTION', 'pgsql')) {
             $id_seq = $table_name . '_id_seq';
-            DB::select("SELECT setval('?', (SELECT MAX(id) from '?'));", [$id_seq, $table_name]);
+            DB::select("SELECT setval('?', (SELECT MAX(id) from ?));", [$id_seq, $table_name]);
         }
     }
 }
