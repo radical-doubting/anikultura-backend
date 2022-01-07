549b2c332cc343b3d394aa049da3d683f4e24929
jg-rivera <rivera.jga10@gmail.com>
2022-01-07 17:52:59 +0800

    Fixed missing redirect

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/admin/main';
 
     /**
      * The controller namespace for the application.
