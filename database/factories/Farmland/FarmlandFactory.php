9b3815bb3ef640ae8ee55ae92e80feb9429f5f29
jg-rivera <rivera.jga10@gmail.com>
2022-02-23 16:26:47 +0800

    Adjusted hectares size seeding in farmland

--- database/factories/Farmland/FarmlandFactory.php
+++ database/factories/Farmland/FarmlandFactory.php
@@ -27,7 +27,7 @@ class FarmlandFactory extends Factory
         return [
             'name' => $this->faker->state . ' Farmland',
             'batch_id' => Batch::all()->random()->id,
-            'hectares_size' => $this->faker->numberBetween(40, 1000),
+            'hectares_size' => $this->faker->randomFloat(2, 1, 10),
             'type_id' => FarmlandType::all()->random()->id,
             'status_id' => FarmlandStatus::all()->random()->id,
         ];
