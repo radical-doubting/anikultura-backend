4be1d76fbfd9d363952f2c100820adcbb70797cf
jg-rivera <rivera.jga10@gmail.com>
2022-02-23 16:18:40 +0800

    Added main screen and anikultura config

---
+++ config/anikultura.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Program Names
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the agricultural program names that the platform will
+    | use on its management dashboard.
+    |
+    */
+
+    'programFullName' => env('ANIKULTURA_PROGRAM_FULL_NAME', 'Anikultura Program'),
+    'programDisplayName' => env('ANIKULTURA_PROGRAM_DISPLAY_NAME', 'Anikultura Program'),
+];
