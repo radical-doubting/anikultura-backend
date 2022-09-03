67d7a54180ae01d3fb4082f8afc8eb97e8193be0
yucabeltran <mariayucabeltran02@gmail.com>
2022-09-03 10:34:50 +0800

    feat: localization feature

---
+++ app/Http/Middleware/Languages.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class Languages
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Session()->has('applocale') AND array_key_exists(Session()->get('applocale'), config('languages'))) {
+            App::setLocale(Session()->get('applocale'));
+        } else {
+            App::setlocale(config('app.fallback_locale'));
+        }
+        return $next($request);
+    }
+}
