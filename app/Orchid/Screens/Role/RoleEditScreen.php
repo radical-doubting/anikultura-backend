6eccf76286045787aaff1dff2f91fae31ae1b1bf
AldwinVergara <advergara@student.apc.edu.ph>
2022-09-07 16:25:34 +0800

    Updates regarding Roles Edit/Create/List screens

--- app/Orchid/Screens/Role/RoleEditScreen.php
+++ app/Orchid/Screens/Role/RoleEditScreen.php
@@ -88,15 +88,13 @@ class RoleEditScreen extends Screen
                 RoleEditLayout::class,
             ])
                 ->title('Role')
-                ->description('A role is a collection of privileges
-                (of possibly different services like the Users service, Moderator, and so on)
-                 that grants users with that role the ability to perform certain tasks or operations.'),
+                ->description(__('A role is a collection of privileges (of possibly different services like the Users service, Moderator, and so on) that grants users with that role the ability to perform certain tasks or operations.')),
 
             Layout::block([
                 RolePermissionLayout::class,
             ])
-                ->title('Permission/Privilege')
-                ->description('A privilege is necessary to perform certain tasks and operations in an area.'),
+                ->title(__('Permission/Privilege'))
+                ->description(__('A privilege is necessary to perform certain tasks and operations in an area.')),
         ];
     }
 
