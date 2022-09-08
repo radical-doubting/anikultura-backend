d7fcb6c409a496a680fd2ce734ab44b8342cb9bd
AldwinVergara <advergara@student.apc.edu.ph>
2022-09-08 12:05:58 +0800

    Updated Tagalog Translation of Batches

--- app/Orchid/Layouts/Batch/BatchSeedAllocationListLayout.php
+++ app/Orchid/Layouts/Batch/BatchSeedAllocationListLayout.php
@@ -68,7 +68,7 @@ class BatchSeedAllocationListLayout extends Table
                         ]);
                 }),
 
-            TD::make('updated_at', __('Last edit'))
+            TD::make('updated_at', __('Last Edit'))
                 ->sort()
                 ->render(function (BatchSeedAllocation $batchSeedAllocation) {
                     return $batchSeedAllocation->updated_at->toDateTimeString();
