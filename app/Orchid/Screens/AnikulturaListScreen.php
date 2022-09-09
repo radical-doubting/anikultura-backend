9163ceec8f5a11a59f64502eaeba95d053641f2e
jg-rivera <rivera.jga10@gmail.com>
2022-09-09 15:19:11 +0800

    Fix get list screen name

--- app/Orchid/Screens/AnikulturaListScreen.php
+++ app/Orchid/Screens/AnikulturaListScreen.php
@@ -10,7 +10,7 @@ abstract class AnikulturaListScreen extends Screen
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
-            $screenName = strtolower($this->name);
+            $screenName = strtolower($this->name());
             $programName = Config::get('anikultura.programFullName');
 
             $this->description = (__('A list of all ')).$screenName.(__(' under the ')).$programName;
