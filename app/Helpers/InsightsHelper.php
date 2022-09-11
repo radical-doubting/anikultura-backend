d8c5281b28e5a53a398e31dd43798945061d3ca8
jg-rivera <rivera.jga10@gmail.com>
2022-09-11 16:04:46 +0800

    Change values to float

--- app/Helpers/InsightsHelper.php
+++ app/Helpers/InsightsHelper.php
@@ -4,12 +4,12 @@ namespace App\Helpers;
 
 class InsightsHelper
 {
-    public static function incrementGauge(string $name, array $labels = [], int $increment = 1): void
+    public static function incrementGauge(string $name, array $labels = [], float $increment = 1): void
     {
         app('prometheus')->getGauge($name)->incBy($increment, $labels);
     }
 
-    public static function decrementGauge(string $name, array $labels = [], int $decrement = 1): void
+    public static function decrementGauge(string $name, array $labels = [], float $decrement = 1): void
     {
         app('prometheus')->getGauge($name)->decBy($decrement, $labels);
     }
