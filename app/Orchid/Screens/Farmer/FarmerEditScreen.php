3e9a961f27545db65e789b41b68b0b375c99d854
jg-rivera <rivera.jga10@gmail.com>
2022-09-14 17:51:20 +0800

    Refactor farmer edit screen

--- app/Orchid/Screens/Farmer/FarmerEditScreen.php
+++ app/Orchid/Screens/Farmer/FarmerEditScreen.php
@@ -5,98 +5,72 @@ namespace App\Orchid\Screens\Farmer;
 use App\Actions\Farmer\CreateFarmer;
 use App\Actions\Farmer\DeleteFarmer;
 use App\Models\Farmer\Farmer;
+use App\Models\Farmer\FarmerAddress;
+use App\Models\Farmer\FarmerProfile;
+use App\Orchid\Layouts\Farmer\FarmerEditAccountLayout;
 use App\Orchid\Layouts\Farmer\FarmerEditAddressLayout;
 use App\Orchid\Layouts\Farmer\FarmerEditJobEducationLayout;
 use App\Orchid\Layouts\Farmer\FarmerEditPersonalLayout;
 use App\Orchid\Layouts\Farmer\FarmerEditSalaryLayout;
-use App\Orchid\Layouts\User\UserEditLayout;
+use App\Orchid\Screens\AnikulturaEditScreen;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Orchid\Screen\Actions\Button;
-use Orchid\Screen\Screen;
 use Orchid\Support\Color;
 use Orchid\Support\Facades\Layout;
 
-class FarmerEditScreen extends Screen
+class FarmerEditScreen extends AnikulturaEditScreen
 {
-    protected $exists = false;
+    public Farmer $farmer;
 
-    public function __construct()
+    public FarmerProfile $farmerProfile;
+
+    public FarmerAddress $farmerAddress;
+
+    public function resourceName(): string
     {
-        $this->name = __('Enroll Farmer');
-        $this->description = __('Enroll a new farmer');
+        return __('farmer');
     }
 
-    /**
-     * Query data.
-     *
-     * @return array
-     */
+    public function exists(): bool
+    {
+        return $this->farmer->exists;
+    }
+
     public function query(Farmer $farmer): array
     {
-        $this->exists = $farmer->exists;
-
-        if ($this->exists) {
-            $this->name = __('Edit Farmer');
-            $this->description = __('Edit farmer details');
-        }
-
         $farmerProfile = $farmer->profile;
-        $farmerAddress = $this->exists ? $farmerProfile->farmerAddress : null;
+        $farmerAddress = $farmerProfile->farmerAddress;
 
         return [
-            'user' => $farmer,
-            'farmer_profile' => $farmerProfile,
-            'farmer_address' => $farmerAddress,
+            'farmer' => $farmer,
+            'farmerProfile' => $farmerProfile,
+            'farmerAddress' => $farmerAddress,
         ];
     }
 
-    /**
-     * Button commands.
-     *
-     * @return \Orchid\Screen\Action[]
-     */
-    public function commandBar(): array
+    public function layout(): iterable
     {
         return [
-            Button::make(__('Remove'))
-                ->icon('trash')
-                ->confirm(__('Once the farmer profile is deleted, all of its resources and data will be permanently deleted.'))
-                ->method('remove')
-                ->canSee($this->exists),
-
-            Button::make(__('Save'))
-                ->icon('check')
-                ->method('save'),
-        ];
-    }
-
-    /**
-     * Views.
-     *
-     * @return \Orchid\Screen\Layout[]|string[]
-     */
-    public function layout(): array
-    {
-        return [
-            Layout::block(UserEditLayout::class)
-                ->title('Account Information')
-                ->description("This information collects farmer's account information."),
+            Layout::block(FarmerEditAccountLayout::class)
+                ->title(__('Account Information'))
+                ->description(__("This information collects farmer's account information.")),
 
             Layout::block(FarmerEditPersonalLayout::class)
-                ->title('Personal Information')
-                ->description("This information collects farmer's personal information."),
+                ->title(__('Personal Information'))
+                ->description(__("This information collects farmer's personal information.")),
 
             Layout::block(FarmerEditAddressLayout::class)
-                ->title('Address Information')
-                ->description("This information collects farmer's address information."),
+                ->title(__('Address Information'))
+                ->description(__("This information collects farmer's address information.")),
 
             Layout::block(FarmerEditJobEducationLayout::class)
-                ->title('Job and Education Information')
-                ->description("This information collects farmer's job and education information."),
+                ->title(__('Job and Education Information'))
+                ->description(__("This information collects farmer's job and education information.")),
 
             Layout::block(FarmerEditSalaryLayout::class)
-                ->title('Salary Information')
-                ->description("This information collects farmer's salary information.")
+                ->title(__('Salary Information'))
+                ->description(__("This information collects farmer's salary information."))
                 ->commands(
                     Button::make(__('Save'))
                         ->type(Color::DEFAULT())
@@ -106,27 +80,12 @@ class FarmerEditScreen extends Screen
         ];
     }
 
-    /**
-     * Save a farmer.
-     *
-     * @param  Farmer  $farmer
-     * @param  Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function save(Farmer $farmer, Request $request)
+    public function save(Farmer $farmer, Request $request): RedirectResponse
     {
         return CreateFarmer::runOrchidAction($farmer, $request);
     }
 
-    /**
-     * Removes a farmer.
-     *
-     * @param  Farmer  $farmer
-     * @return \Illuminate\Http\RedirectResponse
-     *
-     * @throws \Exception
-     */
-    public function remove(Farmer $farmer)
+    public function remove(Farmer $farmer): RedirectResponse
     {
         return DeleteFarmer::runOrchidAction($farmer, null);
     }
