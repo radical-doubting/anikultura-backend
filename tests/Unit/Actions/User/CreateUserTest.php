fe1c9da3294f984c1e7bfdcf659e2b76d7f6cc80
jg-rivera <rivera.jga10@gmail.com>
2022-09-16 15:51:25 +0800

    Add password user unit test

---
+++ tests/Unit/Actions/User/CreateUserTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\Actions\User;
+
+use App\Actions\Authentication\HashPassword;
+use App\Actions\User\CreateUser;
+use App\Models\User;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class CreateUserTest extends TestCase
+{
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function testShouldAvoidUpdatingEmptyPassword(): void
+    {
+        /**
+         * @var \Mockery\Mock&User
+         */
+        $mockUser = Mockery::mock(User::class)->makePartial();
+        $mockUser->shouldReceive('save')->once()->andReturn(true);
+        $mockUser->password = 'hashedPassword';
+
+        $hashPassword = Mockery::mock(HashPassword::class);
+        $hashPassword->shouldNotReceive('handle');
+
+        $createUser = new CreateUser($hashPassword);
+
+        $createdUser = $createUser->handle($mockUser, [
+            'password' => '',
+        ]);
+
+        $this->assertEquals($createdUser->password, 'hashedPassword');
+    }
+}
