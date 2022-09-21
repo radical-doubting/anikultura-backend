552fea98bac3c705e30b256c5f8ea2bf9c922681
jg-rivera <rivera.jga10@gmail.com>
2022-09-21 15:00:57 +0800

    Normalize farmer profiles table

--- database/migrations/2021_06_06_033907_create_farmer_profiles_table.php
+++ database/migrations/2021_06_06_033907_create_farmer_profiles_table.php
@@ -15,25 +15,55 @@ return new class extends Migration
     {
         Schema::create('farmer_profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('gender');
-            $table->string('civil_status');
             $table->date('birthday');
-            $table->tinyInteger('age');
             $table->tinyInteger('quantity_family_members');
             $table->tinyInteger('quantity_dependents');
             $table->tinyInteger('quantity_working_dependents');
-            $table->string('highest_educational_status');
-            $table->string('college_course');
-            $table->string('current_job');
+            $table->string('college_course')->nullable();
+            $table->string('current_job')->nullable();
+            $table->double('estimated_salary');
+
             $table->tinyInteger('farming_years');
             $table->string('usual_crops_planted');
-            $table->string('affiliated_organization');
-            $table->string('tesda_training_joined');
-            $table->string('nc_passer_status');
-            $table->string('salary_periodicity');
-            $table->integer('estimated_salary');
-            $table->string('social_status');
+            $table->string('affiliated_organization')->nullable();
+            $table->string('tesda_training_joined')->nullable();
+            $table->boolean('nc_passer_status');
             $table->string('social_status_reason');
+
+            $table->unsignedBigInteger('gender_id');
+            $table->foreign('gender_id')
+                ->references('id')
+                ->on('genders')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->unsignedBigInteger('civil_status_id');
+            $table->foreign('civil_status_id')
+                ->references('id')
+                ->on('civil_statuses')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->unsignedBigInteger('educational_status_id');
+            $table->foreign('educational_status_id')
+                ->references('id')
+                ->on('educational_statuses')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->unsignedBigInteger('salary_periodicity_id');
+            $table->foreign('salary_periodicity_id')
+                ->references('id')
+                ->on('salary_periodicities')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+
+            $table->unsignedBigInteger('social_status_id');
+            $table->foreign('social_status_id')
+                ->references('id')
+                ->on('social_statuses')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
 
             $table->timestamps();
         });
