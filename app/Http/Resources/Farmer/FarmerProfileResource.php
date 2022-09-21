d253285673d217b6f51d055a0cbf798b3d32c2ed
jg-rivera <rivera.jga10@gmail.com>
2022-09-21 15:38:42 +0800

    Fix type error in resource

--- app/Http/Resources/Farmer/FarmerProfileResource.php
+++ app/Http/Resources/Farmer/FarmerProfileResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Farmer;
 
+use App\Models\Farmer\Gender;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
@@ -11,11 +12,15 @@ class FarmerProfileResource extends JsonResource
 {
     public function toArray($request)
     {
+        /**
+         * @var Gender
+         */
+        $gender = $this->gender;
+
         return [
             'isTutorialDone' => $this->tutorial_done,
-            'gender' => $this->gender,
+            'gender' => $gender->name,
             'birthday' => $this->birthday,
-            'age' => $this->age,
             'affiliatedOrganization' => $this->affiliated_organization,
             'tesdaTrainingJoined' => $this->tesda_training_joined,
             'joinedAt' => $this->created_at,
