d8e49d0eb8c903770a55c63fc0133a1d0bd7b3ed
AldwinVergara <advergara@student.apc.edu.ph>
2022-09-22 13:53:24 +0800

    Composer Update

--- app/Notifications/SendReadyForHarvestNotification.php
+++ app/Notifications/SendReadyForHarvestNotification.php
@@ -7,6 +7,7 @@ use Illuminate\Notifications\Notification;
 use App\Models\FarmerReport\FarmerReport;
 use Orchid\Platform\Notifications\DashboardMessage;
 use Orchid\Platform\Notifications\DashboardChannel;
+use Orchid\Screen\Actions\Link;
 
 class SendReadyForHarvestNotification extends Notification
 {
@@ -17,6 +18,7 @@ class SendReadyForHarvestNotification extends Notification
     public function __construct(FarmerReport $farmerReport)
     {
         $this->farmerReport = $farmerReport;
+        $this->reportId = $farmerReport->id;
     }
 
     public function via($notifiable)
@@ -30,6 +32,7 @@ class SendReadyForHarvestNotification extends Notification
         
         return (new DashboardMessage())
             ->title($fullName . " is ready for harvest!")
-            ->message($this->farmerReport->farmland->name,);
+            ->message($this->farmerReport->farmland->name,)
+            ->action(url('/'));
     }
 }
