2d3a54f1622e1bbd0f2af8bac308a629f536f02f
jg-rivera <rivera.jga10@gmail.com>
2022-09-22 13:21:20 +0800

    Add farmer profile relationship

--- app/Models/Farmer/FarmerPreference.php
+++ app/Models/Farmer/FarmerPreference.php
@@ -4,6 +4,7 @@ namespace App\Models\Farmer;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FarmerPreference extends Model
 {
@@ -14,4 +15,9 @@ class FarmerPreference extends Model
         'tutorial_done',
         'language',
     ];
+
+    public function farmerProfile(): BelongsTo
+    {
+        return $this->belongsTo(FarmerProfile::class);
+    }
 }
