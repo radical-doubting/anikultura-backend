344445cec0621d03f37a826b3e6f87fd86b08a0b
AldwinVergara <advergara@student.apc.edu.ph>
2022-09-24 21:11:05 +0800

    Added Route for Notification

--- app/Notifications/SendReadyForHarvestNotification.php
+++ app/Notifications/SendReadyForHarvestNotification.php
@@ -12,6 +12,7 @@ use Orchid\Screen\Actions\Link;
 class SendReadyForHarvestNotification extends Notification
 {
     use Queueable;
+    protected $target = 'farmer_reports';
 
     public $farmerReport;
 
@@ -33,6 +34,6 @@ class SendReadyForHarvestNotification extends Notification
         return (new DashboardMessage())
             ->title($fullName . " is ready for harvest!")
             ->message($this->farmerReport->farmland->name,)
-            ->action(url('/'));
+            ->action(route('platform.farmer-reports.edit', [$this->farmerReport->id]));
     }
 }
