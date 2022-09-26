08011685c3cebc84c8dfd01058480bfc37152dd0
jg-rivera <rivera.jga10@gmail.com>
2022-09-26 15:33:05 +0800

    Add generic photo url migration
    This ensures that there is no tight coupling with a storage provider.

---
+++ database/migrations/2022_09_26_070926_add_photo_url_to_farmer_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('farmer_reports', function (Blueprint $table) {
+            $table->string('photo_url')
+                ->nullable()
+                ->after('verified');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('farmer_reports', function (Blueprint $table) {
+            $table->dropColumn('photo_url');
+        });
+    }
+};
