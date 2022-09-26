3272b9d29d5ff76191aebcbefffdc0dd894193a3
jg-rivera <rivera.jga10@gmail.com>
2022-09-26 14:43:51 +0800

    Remove references to farmer profile language

--- app/Http/Resources/Farmer/LanguageResource.php
+++ app/Http/Resources/Farmer/LanguageResource.php
@@ -5,14 +5,14 @@ namespace App\Http\Resources\Farmer;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
- * @mixin \App\Models\Farmer
+ * @mixin \App\Models\Farmer\Farmer
  */
 class LanguageResource extends JsonResource
 {
     public function toArray($request)
     {
         return [
-            'language' => $this->language,
+            'language' => $this->preferredLocale(),
         ];
     }
 }
