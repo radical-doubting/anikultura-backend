78044d33c70f0e92762fa7da63664fbd9e92276f
jg-rivera <rivera.jga10@gmail.com>
2022-09-30 13:40:56 +0800

    Add more test cases for province edit screen

--- tests/Feature/Orchid/Screens/Site/Province/ProvinceEditScreenTest.php
+++ tests/Feature/Orchid/Screens/Site/Province/ProvinceEditScreenTest.php
@@ -6,6 +6,8 @@ use Database\Seeders\Admin\AdminProfileSeeder;
 use Database\Seeders\Admin\AdminSeeder;
 use Database\Seeders\Site\RegionSeeder;
 use Database\Seeders\User\RoleSeeder;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
 use function Pest\Laravel\seed;
 
 beforeEach(function () {
@@ -21,12 +23,12 @@ it('shows create screen', function () {
     $screen = screen('platform.sites.provinces.create')->actingAs(Admin::first());
 
     $screen->display()
-        ->assertSee(__('Create'))
-        ->assertSee(__('Province Information'))
-        ->assertSee(__('Save'));
+        ->assertSee('Create province')
+        ->assertSee('Province Information')
+        ->assertSee('Save');
 });
 
-it('shows edit screen', function () {
+it('shows an existing province from the edit screen', function () {
     $province = Province::factory()->count(1)->create()[0];
 
     $screen = screen('platform.sites.provinces.edit')
@@ -34,7 +36,40 @@ it('shows edit screen', function () {
         ->actingAs(Admin::first());
 
     $screen->display()
-        ->assertSee(__('Edit province'))
-        ->assertSee(__('Remove'))
-        ->assertSee(__('Save'));
+        ->assertSee('Edit province')
+        ->assertSee('Remove')
+        ->assertSee('Save')
+        ->assertSee($province->name)
+        ->assertSee($province->region->name);
+});
+
+it('creates a province from the create screen', function () {
+    $province = Province::factory()->count(1)->create()[0];
+    $provinceData = $province->only('name', 'region_id');
+
+    $screen = screen('platform.sites.provinces.create')
+        ->actingAs(Admin::first());
+
+    $screen
+        ->method('save', [
+            'province' => $provinceData,
+        ])
+        ->assertSee('Province was saved successfully!');
+
+    assertDatabaseHas('provinces', $provinceData);
+});
+
+it('deletes an existing province from the edit screen', function () {
+    $province = Province::factory()->count(1)->create()[0];
+    $provinceData = $province->only('name', 'region_id');
+
+    $screen = screen('platform.sites.provinces.edit')
+        ->parameters([$province->id])
+        ->actingAs(Admin::first());
+
+    $screen
+        ->method('remove')
+        ->assertSee('Province was removed successfully!');
+
+    assertDatabaseMissing('provinces', $provinceData);
 });
