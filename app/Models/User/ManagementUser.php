a1a683abd352f07ac123668ba12b12b4bb6c063f
jg-rivera <rivera.jga10@gmail.com>
2022-10-01 15:01:45 +0800

    Move user and management user model to user folder

--- app/Models/User/ManagementUser.php
+++ app/Models/User/ManagementUser.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\User;
 
 use App\Models\Admin\Admin;
 use App\Models\BigBrother\BigBrother;
--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\User;
 
 use App\Traits\Loggable;
 use Illuminate\Contracts\Translation\HasLocalePreference;
