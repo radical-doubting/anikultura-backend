e29ab17847102767ca044ad6f0c929a429af26f2
jg-rivera <rivera.jga10@gmail.com>
2022-10-03 15:45:25 +0800

    Add make Orchid link action helper function

--- app/Helpers/InsightsHelper.php
+++ app/Helpers/InsightsHelper.php
@@ -2,6 +2,8 @@
 
 namespace App\Helpers;
 
+use Orchid\Screen\Actions\Link;
+
 class InsightsHelper
 {
     public static function incrementGauge(string $name, array $labels = [], float $increment = 1): void
@@ -12,5 +14,17 @@ class InsightsHelper
     public static function decrementGauge(string $name, array $labels = [], float $decrement = 1): void
     {
         app('prometheus')->getGauge($name)->decBy($decrement, $labels);
+    }
+
+    public static function makeLink(string $resource = 'home'): Link
+    {
+        $link = config("anikultura.insightsUrl.$resource");
+        $isDefault = $link === '#';
+
+        return Link::make(__('View Insights'))
+            ->icon('bulb')
+            ->href($link)
+            ->target('_blank')
+            ->canSee(! $isDefault);
     }
 }
