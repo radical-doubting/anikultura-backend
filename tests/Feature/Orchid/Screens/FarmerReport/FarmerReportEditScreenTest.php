20f4a7dbc23ec206e7f2585314d83b6828bd8ceb
jg-rivera <rivera.jga10@gmail.com>
2022-10-04 20:31:18 +0800

    Add seed planted test case in farmer report test

--- tests/Feature/Orchid/Screens/FarmerReport/FarmerReportEditScreenTest.php
+++ tests/Feature/Orchid/Screens/FarmerReport/FarmerReportEditScreenTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Crop\SeedStage;
 use App\Models\FarmerReport\FarmerReport;
 use App\Models\User\Admin\Admin;
 use App\Models\User\Farmer\Farmer;
@@ -58,6 +59,28 @@ it('shows an existing farmer report from the edit screen', function () {
         ->assertSee('Remove')
         ->assertSee('Save')
         ->assertSee($farmerReport->farmer->first_name);
+});
+
+it('shows seed planted stage farmer report with disclaimer from the edit screen', function () {
+    $farmerReport = FarmerReport::factory()->createOne([
+        'reported_by' => Farmer::first()->id,
+        'seed_stage_id' => SeedStage::firstWhere('slug', 'seeds-planted')->id,
+    ]);
+
+    $screen = screen('platform.farmer-reports.edit')
+        ->parameters([$farmerReport->id])
+        ->actingAs(Admin::first());
+
+    $screen->display()
+        ->assertSee('Edit farmer report')
+        ->assertSee('Report Information')
+        ->assertSee('Attachment Information')
+        ->assertSee('Estimation Information')
+        ->assertSee('Verification')
+        ->assertSee('Remove')
+        ->assertSee('Save')
+        ->assertSee($farmerReport->farmer->first_name)
+        ->assertSee('The estimated yield assuming');
 });
 
 it('creates a farmer report from the create screen', function () {
