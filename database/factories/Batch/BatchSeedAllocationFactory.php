2d5d125eb38ae12809178a6ac02d0422053cf441
jg-rivera <rivera.jga10@gmail.com>
2022-10-04 18:08:25 +0800

    Add more fields in batch seed allocation factory

--- database/factories/Batch/BatchSeedAllocationFactory.php
+++ database/factories/Batch/BatchSeedAllocationFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories\Batch;
 
+use App\Models\Batch\Batch;
+use App\Models\Crop\Crop;
+use App\Models\User\Farmer\Farmer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,6 +20,9 @@ class BatchSeedAllocationFactory extends Factory
     public function definition()
     {
         return [
+            'batch_id' => Batch::all()->random()->id,
+            'farmer_id' => Farmer::all()->random()->id,
+            'crop_id' => Crop::all()->random()->id,
             'seed_amount' => $this->faker->numberBetween(100, 1000),
         ];
     }
