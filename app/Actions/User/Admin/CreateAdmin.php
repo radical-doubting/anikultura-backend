7a4bf74b9a5a1fdd6d70ee3fd608cfefd77c5740
jg-rivera <rivera.jga10@gmail.com>
2022-10-05 20:39:08 +0800

    Add admin actions

---
+++ app/Actions/User/Admin/CreateAdmin.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Actions\User\Admin;
+
+use App\Actions\User\CreateUser;
+use App\Models\User\Admin\Admin;
+use App\Models\User\Admin\AdminProfile;
+use App\Models\User\User;
+use App\Traits\AsOrchidAction;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Orchid\Support\Facades\Toast;
+
+class CreateAdmin
+{
+    use AsAction;
+    use AsOrchidAction;
+
+    public function __construct(
+        protected CreateUser $createUser,
+        protected CreateAdminProfile $createAdminProfile,
+    ) {
+    }
+
+    public function handle(Admin $admin, array $adminData): Admin
+    {
+        $createdAccount = $this->createUser->handle(
+            $admin,
+            $adminData['account']
+        );
+
+        $adminProfile = $this->createProfileOrUpdate($createdAccount);
+
+        $updatedAdminProfile = $this->createAdminProfile->handle(
+            $adminProfile,
+            $adminData['profile']
+        );
+
+        $this->updateProfileType($createdAccount, $updatedAdminProfile);
+
+        return $admin->refresh();
+    }
+
+    private function createProfileOrUpdate(User $createdAccount): AdminProfile
+    {
+        $adminProfile = $createdAccount->profile;
+
+        return is_null($adminProfile) ? new AdminProfile() : $adminProfile;
+    }
+
+    private function updateProfileType(User $createdAccount, AdminProfile $adminProfile)
+    {
+        $createdAccount->update([
+            'profile_id' => $adminProfile->id,
+            'profile_type' => Admin::PROFILE_PATH,
+        ]);
+    }
+
+    public function asOrchidAction(mixed $model, ?Request $request): RedirectResponse
+    {
+        $this->validateIfBigBrotherAccountExistsAlready($model, $request);
+
+        $this->handle($model, [
+            'account' => $request->get('admin'),
+            'profile' => $request->get('adminProfile'),
+        ]);
+
+        Toast::info(__('Administrator was saved successfully!'));
+
+        return redirect()->route('platform.admins');
+    }
+
+    private function validateIfBigBrotherAccountExistsAlready($admin, Request $request)
+    {
+        $userNameShouldBeUnique = Rule::unique(Admin::class, 'name')->ignore($admin);
+        $emailShouldBeUnique = Rule::unique(Admin::class, 'email')->ignore($admin);
+
+        $request->validate([
+            'admin.name' => [
+                'required',
+                $userNameShouldBeUnique,
+            ],
+            'admin.email' => [
+                $emailShouldBeUnique,
+            ],
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'adminProfile.age' => [
+                'required',
+            ],
+        ];
+    }
+}
---
+++ app/Actions/User/Admin/DeleteAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Actions\User\Admin;
+
+use App\Models\User\Admin\Admin;
+use App\Traits\AsOrchidAction;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Orchid\Support\Facades\Toast;
+
+class DeleteAdmin
+{
+    use AsAction;
+    use AsOrchidAction;
+
+    public function handle(Admin $admin): bool
+    {
+        return $admin->delete();
+    }
+
+    public function asOrchidAction(mixed $model, ?Request $request): RedirectResponse
+    {
+        $this->handle($model);
+
+        Toast::info(__('Administrator was removed successfully!'));
+
+        return redirect()->route('platform.admins');
+    }
+}
