871d5c60a20ec041d0b724de080ee5e0e8af7c4a
jg-rivera <rivera.jga10@gmail.com>
2022-10-11 18:19:57 +0800

    Add farmer validation rules

--- app/Actions/User/Farmer/CreateFarmer.php
+++ app/Actions/User/Farmer/CreateFarmer.php
@@ -115,96 +115,151 @@ class CreateFarmer
         return [
             'farmerProfile.gender_id' => [
                 'required',
+                'integer',
+                'exists:genders,id',
             ],
 
             'farmerProfile.civil_status_id' => [
                 'required',
+                'integer',
+                'exists:civil_statuses,id',
             ],
 
             'farmerProfile.birthday' => [
                 'required',
+                'date',
             ],
 
             'farmerProfile.quantity_family_members' => [
                 'required',
+                'integer',
+                'min:0',
+                'max:30',
             ],
 
             'farmerProfile.quantity_dependents' => [
                 'required',
+                'integer',
+                'min:0',
+                'max:30',
             ],
 
             'farmerProfile.quantity_working_dependents' => [
                 'required',
+                'integer',
+                'min:0',
+                'max:30',
             ],
 
             'farmerProfile.educational_status_id' => [
                 'required',
+                'integer',
+                'exists:educational_statuses,id',
             ],
 
-            'farmerProfile.college_course' => [],
+            'farmerProfile.college_course' => [
+                'nullable',
+                'alpha_num_space_dash',
+            ],
 
             'farmerProfile.current_job' => [
                 'required',
+                'alpha_num_space_dash',
             ],
 
             'farmerProfile.farming_years' => [
                 'required',
+                'integer',
+                'min:0',
+                'max:100',
             ],
 
             'farmerProfile.usual_crops_planted' => [
                 'required',
+                'string',
             ],
 
             'farmerProfile.affiliated_organization' => [
                 'required',
+                'alpha_num_space_dash',
             ],
 
             'farmerProfile.tesda_training_joined' => [
                 'required',
+                'alpha_num_space_dash',
             ],
 
             'farmerProfile.nc_passer_status_id' => [
                 'required',
+                'integer',
+                'exists:nc_passer_statuses,id',
             ],
 
             'farmerProfile.salary_periodicity_id' => [
                 'required',
+                'integer',
+                'exists:salary_periodicities,id',
             ],
 
             'farmerProfile.estimated_salary' => [
                 'required',
+                'numeric',
+                'min:1',
+                'max:1000000',
             ],
 
             'farmerProfile.social_status_id' => [
                 'required',
+                'integer',
+                'exists:social_statuses,id',
             ],
 
             'farmerProfile.social_status_reason' => [
                 'required',
+                'string',
+                'min:10',
+                'max:255',
             ],
 
             'farmerAddress.house_number' => [
                 'required',
+                'string',
+                'min:1',
+                'max:10',
             ],
 
             'farmerAddress.street' => [
                 'required',
+                'string',
+                'min:3',
+                'max:70',
             ],
 
             'farmerAddress.barangay' => [
                 'required',
+                'alpha_num_space_dash',
+                'min:3',
+                'max:70',
             ],
 
             'farmerAddress.municity' => [
                 'required',
+                'alpha_num_space_dash',
+                'min:3',
+                'max:70',
             ],
 
             'farmerAddress.province' => [
                 'required',
+                'alpha_num_space_dash',
+                'min:3',
+                'max:70',
             ],
 
             'farmerAddress.region_id' => [
                 'required',
+                'integer',
+                'exists:regions,id',
             ],
         ];
     }
