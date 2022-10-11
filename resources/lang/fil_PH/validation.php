a7816bf3b073eaa6b71080b87e64b305b01d39ec
jg-rivera <rivera.jga10@gmail.com>
2022-10-11 16:09:37 +0800

    Add alpha_num_space_dash validation rule

--- resources/lang/fil_PH/validation.php
+++ resources/lang/fil_PH/validation.php
@@ -21,6 +21,7 @@ return [
     'alpha' => 'The :attribute must only contain letters.',
     'alpha_dash' => 'The :attribute must only contain letters, numbers, dashes and underscores.',
     'alpha_num' => 'The :attribute must only contain letters and numbers.',
+    'alpha_num_space_dash' => 'The :attribute may only contain letters, numbers, spaces, and dashes.',
     'array' => 'The :attribute must be an array.',
     'before' => 'The :attribute must be a date before :date.',
     'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
