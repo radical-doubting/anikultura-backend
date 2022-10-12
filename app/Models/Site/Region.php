d231819da1cf291202fec862243f8076ce287b13
yucabeltran <mariayucabeltran02@gmail.com>
2022-10-12 11:45:37 +0800

    Add province relation on region

--- app/Models/Site/Region.php
+++ app/Models/Site/Region.php
@@ -7,6 +7,7 @@ use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Orchid\Filters\Filterable;
+use App\Models\Site\Province;
 
 /**
  * @property string $slug
