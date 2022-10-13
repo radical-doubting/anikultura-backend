f6ebbf272976c7f474271840235626e8ed280c0a
jg-rivera <rivera.jga10@gmail.com>
2022-10-13 17:05:06 +0800

    Make verification layout use select

--- app/Orchid/Layouts/FarmerReport/FarmerReportEditVerificationLayout.php
+++ app/Orchid/Layouts/FarmerReport/FarmerReportEditVerificationLayout.php
@@ -2,21 +2,22 @@
 
 namespace App\Orchid\Layouts\FarmerReport;
 
+use App\Models\FarmerReport\FarmerReportStatus;
 use App\Models\User\ManagementUser;
 use App\Orchid\Layouts\AnikulturaEditLayout;
-use Orchid\Screen\Fields\CheckBox;
 use Orchid\Screen\Fields\Relation;
+use Orchid\Screen\Fields\Select;
 
 class FarmerReportEditVerificationLayout extends AnikulturaEditLayout
 {
     protected function fields(): iterable
     {
         return [
-            CheckBox::make('farmerReport.verified')
-                ->required()
-                ->sendTrueOrFalse()
+            Select::make('farmerReport.status_id')
+                ->fromModel(FarmerReportStatus::class, 'name')
                 ->title(__('Verification Status'))
-                ->placeholder(__('This farmer report is considered as valid')),
+                ->placeholder(__('This farmer report is considered as valid'))
+                ->required(),
 
             Relation::make('farmerReport.verified_by')
                 ->fromModel(ManagementUser::class, 'name')
