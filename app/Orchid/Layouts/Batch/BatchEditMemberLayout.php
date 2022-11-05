83903e6ca245b58c889d915f3663c56b37cf3e11
jg-rivera <rivera.jga10@gmail.com>
2022-11-05 17:35:38 +0800

    Add batch big brothers to member layout

--- app/Orchid/Layouts/Batch/BatchEditMemberLayout.php
+++ app/Orchid/Layouts/Batch/BatchEditMemberLayout.php
@@ -2,6 +2,7 @@
 
 namespace App\Orchid\Layouts\Batch;
 
+use App\Models\User\BigBrother\BigBrother;
 use App\Models\User\Farmer\Farmer;
 use App\Orchid\Layouts\AnikulturaEditLayout;
 use Orchid\Screen\Fields\Relation;
@@ -17,9 +18,19 @@ class BatchEditMemberLayout extends AnikulturaEditLayout
                 ->displayAppend('fullName')
                 ->required()
                 ->multiple()
-                ->help(__('Search the name of this batch\'s members'))
+                ->help(__('Search the name of this batch\'s farmers'))
                 ->title(__('Farmers'))
                 ->placeholder(__('Farmers')),
+
+            Relation::make('batch.bigBrothers.')
+                ->fromModel(BigBrother::class, 'name')
+                ->searchColumns('first_name', 'last_name')
+                ->displayAppend('fullName')
+                ->required()
+                ->multiple()
+                ->help(__('Search the name of this batch\'s big brothers'))
+                ->title(__('Big Brothers'))
+                ->placeholder(__('Big Brothers')),
         ];
     }
 }
