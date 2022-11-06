91fc89cd76c64573a55d09f0c49401d2da712bc8
jg-rivera <rivera.jga10@gmail.com>
2022-11-07 00:19:54 +0800

    Update farmer edit screen test

--- tests/Feature/Orchid/Screens/User/Farmer/FarmerEditScreenTest.php
+++ tests/Feature/Orchid/Screens/User/Farmer/FarmerEditScreenTest.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Models\Batch\Batch;
+use App\Models\Farmland\Farmland;
 use App\Models\User\Admin\Admin;
+use App\Models\User\BigBrother\BigBrother;
 use App\Models\User\Farmer\Farmer;
 use App\Models\User\Farmer\FarmerAddress;
 use App\Models\User\Farmer\FarmerProfile;
-use Database\Seeders\Site\RegionSeeder;
+use Database\Seeders\Farmland\FarmlandStatusSeeder;
+use Database\Seeders\Farmland\FarmlandTypeSeeder;
+use Database\Seeders\Farmland\WateringSystemSeeder;
+use Database\Seeders\Site\SiteSeeder;
 use Database\Seeders\User\Admin\AdminSeeder;
+use Database\Seeders\User\BigBrother\BigBrotherSeeder;
 use Database\Seeders\User\Farmer\CivilStatusSeeder;
 use Database\Seeders\User\Farmer\EducationalStatusSeeder;
 use Database\Seeders\User\Farmer\GenderSeeder;
@@ -22,17 +29,21 @@ beforeEach(function () {
     seed([
         RoleSeeder::class,
         AdminSeeder::class,
-        RegionSeeder::class,
+        BigBrotherSeeder::class,
+        SiteSeeder::class,
         GenderSeeder::class,
         CivilStatusSeeder::class,
         EducationalStatusSeeder::class,
         SalaryPeriodicitySeeder::class,
         SocialStatusSeeder::class,
         NCPasserStatusSeeder::class,
+        FarmlandTypeSeeder::class,
+        FarmlandStatusSeeder::class,
+        WateringSystemSeeder::class,
     ]);
 });
 
-it('shows create screen', function () {
+it('shows create screen as admin', function () {
     $screen = screen('platform.farmers.create')->actingAs(Admin::first());
 
     $screen->display()
@@ -45,7 +56,20 @@ it('shows create screen', function () {
         ->assertSee('Save');
 });
 
-it('shows an existing farmer from the edit screen', function () {
+it('shows create screen as big brother', function () {
+    $screen = screen('platform.farmers.create')->actingAs(BigBrother::first());
+
+    $screen->display()
+        ->assertSee('Enroll farmer')
+        ->assertSee('Account Information')
+        ->assertSee('Personal Information')
+        ->assertSee('Address Information')
+        ->assertSee('Job and Education Information')
+        ->assertSee('Salary Information')
+        ->assertSee('Save');
+});
+
+it('shows any farmer from the edit screen as admin', function () {
     $farmer = Farmer::factory()->createOne();
 
     $screen = screen('platform.farmers.edit')
@@ -65,7 +89,51 @@ it('shows an existing farmer from the edit screen', function () {
         ->assertSee($farmer->last_name);
 });
 
-it('creates a farmer from the create screen', function () {
+it('does not show a non-belonging farmer from the edit screen as big brother', function () {
+    $farmer = Farmer::factory()->createOne();
+
+    $screen = screen('platform.farmers.edit')
+        ->parameters([$farmer->id])
+        ->actingAs(BigBrother::first());
+
+    $screen->display()
+        ->assertStatus(403);
+});
+
+it('shows a belonging farmer from the edit screen as big brother', function () {
+    $bigBrother = BigBrother::first();
+
+    /**
+     * @var Farmer
+     */
+    $farmer = Farmer::factory()->createOne();
+
+    /**
+     * @var Batch
+     */
+    $batch = Batch::factory()->createOne();
+
+    $batch->bigBrothers()->sync($bigBrother);
+    $batch->farmers()->sync($farmer);
+
+    $screen = screen('platform.farmers.edit')
+        ->parameters([$farmer->id])
+        ->actingAs($bigBrother);
+
+    $screen->display()
+        ->assertSee('Edit farmer')
+        ->assertSee('Account Information')
+        ->assertSee('Personal Information')
+        ->assertSee('Address Information')
+        ->assertSee('Job and Education Information')
+        ->assertSee('Salary Information')
+        ->assertSee('Remove')
+        ->assertSee('Save')
+        ->assertSee($farmer->first_name)
+        ->assertSee($farmer->last_name);
+});
+
+it('creates a farmer from the create screen as admin', function () {
     $screen = screen('platform.farmers.create')
         ->actingAs(Admin::first());
 
@@ -115,6 +183,24 @@ it('creates a farmer from the create screen', function () {
         'region_id',
     );
 
+    /**
+     * @var Batch
+     */
+    $batch = Batch::factory()->createOne();
+
+    $farmland = Farmland::factory()->createOne([
+        'batch_id' => $batch->id,
+    ]);
+
+    $farmerAssignmentData = [
+        'batches' => [
+            $batch->id,
+        ],
+        'farmlands' => [
+            $farmland->id,
+        ],
+    ];
+
     $screen
         ->method('save', [
             'farmer' => [
@@ -123,13 +209,107 @@ it('creates a farmer from the create screen', function () {
             ],
             'farmerProfile' => $farmerProfileData,
             'farmerAddress' => $farmerAddressData,
+            'farmerAssignment' => $farmerAssignmentData,
         ])
         ->assertSee('Farmer was saved successfully!');
 
-    assertDatabaseCount('users', 2);
+    assertDatabaseCount('users', 12);
     assertDatabaseHas('users', $farmerData);
     assertDatabaseCount('farmer_profiles', 1);
     assertDatabaseCount('farmer_addresses', 1);
+    assertDatabaseCount('batch_farmers', 1);
+    assertDatabaseCount('farmland_farmers', 1);
+});
+
+it('creates a farmer from the create screen as big brother', function () {
+    $bigBrother = BigBrother::first();
+
+    $screen = screen('platform.farmers.create')
+        ->actingAs($bigBrother);
+
+    /**
+     * @var Farmer
+     */
+    $farmer = Farmer::factory()->makeOne();
+
+    $farmerData = $farmer->only(
+        'name',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'email',
+        'contact_number'
+    );
+
+    $farmerProfile = FarmerProfile::factory()->makeOne();
+    $farmerProfileData = $farmerProfile->only(
+        'gender_id',
+        'civil_status_id',
+        'birthday',
+        'quantity_family_members',
+        'quantity_dependents',
+        'quantity_working_dependents',
+        'educational_status_id',
+        'college_course',
+        'current_job',
+        'farming_years',
+        'usual_crops_planted',
+        'affiliated_organization',
+        'tesda_training_joined',
+        'nc_passer_status_id',
+        'salary_periodicity_id',
+        'estimated_salary',
+        'social_status_id',
+        'social_status_reason',
+    );
+
+    $farmerAddress = FarmerAddress::factory()->makeOne();
+    $farmerAddressData = $farmerAddress->only(
+        'house_number',
+        'street',
+        'barangay',
+        'municity',
+        'province',
+        'region_id',
+    );
+
+    /**
+     * @var Batch
+     */
+    $batch = Batch::factory()->createOne();
+    $batch->bigBrothers()->sync($bigBrother);
+
+    $farmland = Farmland::factory()->createOne([
+        'batch_id' => $batch->id,
+    ]);
+
+    $farmerAssignmentData = [
+        'batches' => [
+            $batch->id,
+        ],
+        'farmlands' => [
+            $farmland->id,
+        ],
+    ];
+
+    $screen
+        ->method('save', [
+            'farmer' => [
+                ...$farmerData,
+                'password' => 'SuperSecurePassword1!',
+            ],
+            'farmerProfile' => $farmerProfileData,
+            'farmerAddress' => $farmerAddressData,
+            'farmerAssignment' => $farmerAssignmentData,
+        ])
+        ->assertSee('Farmer was saved successfully!');
+
+    assertDatabaseCount('users', 12);
+    assertDatabaseHas('users', $farmerData);
+    assertDatabaseCount('farmer_profiles', 1);
+    assertDatabaseCount('farmer_addresses', 1);
+    assertDatabaseCount('batch_farmers', 1);
+    assertDatabaseCount('farmland_farmers', 1);
 });
 
 it('deletes an existing farmer from the edit screen', function () {
@@ -160,7 +340,7 @@ it('deletes an existing farmer from the edit screen', function () {
         ->method('remove')
         ->assertSee('Farmer was removed successfully!');
 
-    assertDatabaseCount('users', 1);
+    assertDatabaseCount('users', 11);
     assertDatabaseMissing('users', $farmerData);
     assertDatabaseCount('farmer_profiles', 0);
     assertDatabaseCount('farmer_addresses', 0);
