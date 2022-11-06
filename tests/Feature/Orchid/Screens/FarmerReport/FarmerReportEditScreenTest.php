bf1a008eb031929f43f16e65ea16c9174e0e448d
jg-rivera <rivera.jga10@gmail.com>
2022-11-06 20:15:59 +0800

    Update farmer report screen tests

--- tests/Feature/Orchid/Screens/FarmerReport/FarmerReportEditScreenTest.php
+++ tests/Feature/Orchid/Screens/FarmerReport/FarmerReportEditScreenTest.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Models\Batch\Batch;
 use App\Models\Crop\SeedStage;
 use App\Models\FarmerReport\FarmerReport;
 use App\Models\User\Admin\Admin;
+use App\Models\User\BigBrother\BigBrother;
 use App\Models\User\Farmer\Farmer;
 use Database\Seeders\Batch\BatchSeeder;
 use Database\Seeders\Crop\CropSeeder;
 use Database\Seeders\FarmerReport\FarmerReportStatusSeeder;
 use Database\Seeders\Farmland\FarmlandSeeder;
 use Database\Seeders\Site\SiteSeeder;
-use Database\Seeders\User\Admin\AdminProfileSeeder;
 use Database\Seeders\User\Admin\AdminSeeder;
+use Database\Seeders\User\BigBrother\BigBrotherSeeder;
 use Database\Seeders\User\Farmer\FarmerSeeder;
 use Database\Seeders\User\RoleSeeder;
+use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\assertDatabaseMissing;
 use function Pest\Laravel\seed;
 
@@ -20,7 +23,7 @@ beforeEach(function () {
     seed([
         RoleSeeder::class,
         AdminSeeder::class,
-        AdminProfileSeeder::class,
+        BigBrotherSeeder::class,
         SiteSeeder::class,
         FarmerSeeder::class,
         CropSeeder::class,
@@ -30,7 +33,7 @@ beforeEach(function () {
     ]);
 });
 
-it('shows an existing farmer report from the edit screen', function () {
+it('shows an existing farmer report from the edit screen as admin', function () {
     $farmerReport = FarmerReport::factory()->createOne([
         'reported_by' => Farmer::first()->id,
     ]);
@@ -50,7 +53,48 @@ it('shows an existing farmer report from the edit screen', function () {
         ->assertSee($farmerReport->farmer->first_name);
 });
 
-it('shows seed planted stage farmer report with disclaimer from the edit screen', function () {
+it('does not show non-belonging farmer report from the edit screen as big brother', function () {
+    $farmerReport = FarmerReport::factory()->createOne([
+        'reported_by' => Farmer::first()->id,
+    ]);
+
+    $screen = screen('platform.farmer-reports.edit')
+        ->parameters([$farmerReport->id])
+        ->actingAs(BigBrother::first());
+
+    $screen->display()
+        ->assertStatus(403);
+});
+
+it('shows an existing farmer report from the edit screen as big brother', function () {
+    $bigBrother = BigBrother::first();
+
+    $farmerReport = FarmerReport::factory()->createOne([
+        'reported_by' => Farmer::first()->id,
+    ]);
+
+    /**
+     * @var Batch
+     */
+    $batch = $farmerReport->farmland->batch;
+    $batch->bigBrothers()->sync($bigBrother);
+
+    $screen = screen('platform.farmer-reports.edit')
+        ->parameters([$farmerReport->id])
+        ->actingAs($bigBrother);
+
+    $screen->display()
+        ->assertSee('Edit farmer report')
+        ->assertSee('Report Information')
+        ->assertSee('Attachment Information')
+        ->assertSee('Estimation Information')
+        ->assertSee('Verification')
+        ->assertDontSee('Remove')
+        ->assertSee('Save')
+        ->assertSee($farmerReport->farmer->first_name);
+});
+
+it('shows seed planted stage farmer report with disclaimer from the edit screen as admin', function () {
     $farmerReport = FarmerReport::factory()->createOne([
         'reported_by' => Farmer::first()->id,
         'seed_stage_id' => SeedStage::firstWhere('slug', 'seeds-planted')->id,
@@ -72,7 +116,37 @@ it('shows seed planted stage farmer report with disclaimer from the edit screen'
         ->assertSee('The estimated yield assuming');
 });
 
-it('deletes an existing farmer report from the edit screen', function () {
+it('shows seed planted stage farmer report with disclaimer from the edit screen as big brother', function () {
+    $bigBrother = BigBrother::first();
+
+    $farmerReport = FarmerReport::factory()->createOne([
+        'reported_by' => Farmer::first()->id,
+        'seed_stage_id' => SeedStage::firstWhere('slug', 'seeds-planted')->id,
+    ]);
+
+    /**
+     * @var Batch
+     */
+    $batch = $farmerReport->farmland->batch;
+    $batch->bigBrothers()->sync($bigBrother);
+
+    $screen = screen('platform.farmer-reports.edit')
+        ->parameters([$farmerReport->id])
+        ->actingAs($bigBrother);
+
+    $screen->display()
+        ->assertSee('Edit farmer report')
+        ->assertSee('Report Information')
+        ->assertSee('Attachment Information')
+        ->assertSee('Estimation Information')
+        ->assertSee('Verification')
+        ->assertDontSee('Remove')
+        ->assertSee('Save')
+        ->assertSee($farmerReport->farmer->first_name)
+        ->assertSee('The estimated yield assuming');
+});
+
+it('deletes any farmer report from the edit screen as admin', function () {
     $farmerReport = FarmerReport::factory()->createOne([
         'reported_by' => Farmer::first()->id,
     ]);
@@ -95,4 +169,29 @@ it('deletes an existing farmer report from the edit screen', function () {
         ->assertSee('Farmer report was removed successfully!');
 
     assertDatabaseMissing('farmer_reports', $farmerReportData);
+});
+
+it('does not delete any farmer report from the edit screen as big brother', function () {
+    $farmerReport = FarmerReport::factory()->createOne([
+        'reported_by' => Farmer::first()->id,
+    ]);
+
+    $farmerReportData = $farmerReport->only(
+        'reported_by',
+        'seed_stage_id',
+        'farmland_id',
+        'crop_id',
+        'volume_kg',
+        'status_id',
+    );
+
+    $screen = screen('platform.farmer-reports.edit')
+        ->parameters([$farmerReport->id])
+        ->actingAs(BigBrother::first());
+
+    $screen
+        ->method('remove')
+        ->assertDontSee('Farmer report was removed successfully!');
+
+    assertDatabaseHas('farmer_reports', $farmerReportData);
 });
