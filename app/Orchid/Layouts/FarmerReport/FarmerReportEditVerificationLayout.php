3bd3a075c57bf00591db4854776799254217f691
jg-rivera <rivera.jga10@gmail.com>
2022-11-06 17:37:42 +0800

    Update report verification layout

--- app/Orchid/Layouts/FarmerReport/FarmerReportEditVerificationLayout.php
+++ app/Orchid/Layouts/FarmerReport/FarmerReportEditVerificationLayout.php
@@ -2,6 +2,7 @@
 
 namespace App\Orchid\Layouts\FarmerReport;
 
+use App\Models\FarmerReport\FarmerReport;
 use App\Models\FarmerReport\FarmerReportStatus;
 use App\Models\User\ManagementUser;
 use App\Orchid\Layouts\AnikulturaEditLayout;
@@ -12,6 +13,10 @@ class FarmerReportEditVerificationLayout extends AnikulturaEditLayout
 {
     protected function fields(): iterable
     {
+        $verifier = $this->query->get('farmerReport.verified_by');
+
+        $hasVerifier = ! is_null($verifier);
+
         return [
             Select::make('farmerReport.status_id')
                 ->fromModel(FarmerReportStatus::class, 'name')
@@ -20,12 +25,14 @@ class FarmerReportEditVerificationLayout extends AnikulturaEditLayout
 
             Relation::make('farmerReport.verified_by')
                 ->fromModel(ManagementUser::class, 'name')
-                ->searchColumns('first_name', 'last_name')
+                ->applyScope('')
                 ->displayAppend('fullNameWithRole')
                 ->required()
                 ->help(__('The big brother or administrator who verified this farming report'))
                 ->title(__('Verified by'))
-                ->placeholder(__('Verified by')),
+                ->placeholder(__('Verified by'))
+                ->canSee($hasVerifier)
+                ->disabled(),
         ];
     }
 }
