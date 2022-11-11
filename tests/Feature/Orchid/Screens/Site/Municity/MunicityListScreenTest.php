41a7ebc5d5a49076dc2f27010845933669f08825
jg-rivera <rivera.jga10@gmail.com>
2022-11-11 13:31:31 +0800

    Update site screen tests

--- tests/Feature/Orchid/Screens/Site/Municity/MunicityListScreenTest.php
+++ tests/Feature/Orchid/Screens/Site/Municity/MunicityListScreenTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Feature\Orchid\Screens\Site\Municity;
 
+use App\Models\Site\Municity;
 use App\Models\User\Admin\Admin;
-use Database\Seeders\User\Admin\AdminProfileSeeder;
+use App\Models\User\BigBrother\BigBrother;
+use Database\Seeders\Site\ProvinceSeeder;
+use Database\Seeders\Site\RegionSeeder;
 use Database\Seeders\User\Admin\AdminSeeder;
+use Database\Seeders\User\BigBrother\BigBrotherSeeder;
 use Database\Seeders\User\RoleSeeder;
 use function Pest\Laravel\seed;
 
@@ -12,13 +16,33 @@ beforeEach(function () {
     seed([
         RoleSeeder::class,
         AdminSeeder::class,
-        AdminProfileSeeder::class,
+        BigBrotherSeeder::class,
+        RegionSeeder::class,
+        ProvinceSeeder::class,
     ]);
 });
 
-it('shows list screen', function () {
+it('shows list screen as admin', function () {
     $screen = screen('platform.sites.municities')->actingAs(Admin::first());
 
     $screen->display()
         ->assertSee(__('Municipalities and Cities'));
+});
+
+it('does not show list screen as big brother', function () {
+    $screen = screen('platform.sites.municities')->actingAs(BigBrother::first());
+
+    $screen->display()
+        ->assertStatus(403);
+});
+
+it('shows municity in list screen as admin', function () {
+    $municity = Municity::factory()->createOne();
+
+    $screen = screen('platform.sites.municities')->actingAs(Admin::first());
+
+    $screen->display()
+        ->assertSee($municity->name)
+        ->assertSee($municity->region->name)
+        ->assertSee($municity->province->name);
 });
